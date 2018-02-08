1ce96d3c2660c081eaa32d940e62ad15b3c2a0f4
Diogo Dutra <dutradda@gmail.com>
2018-02-08 15:46:23 -0200

    WIP: splitting schema.json in multiple files

--- openapi21/__init__.py
+++ openapi21/__init__.py
@@ -1,7 +1,7 @@
 import os.path
 
 OPENAPI21_PATH = os.path.dirname(os.path.realpath(__file__))
-SCHEMA_URL = 'file:{}/schema.json'.format(OPENAPI21_PATH)
+SCHEMA_URL = 'file:{}/spec/schema.json'.format(OPENAPI21_PATH)
 
 from openapi21.validator import (validate_spec_url,
                                  validate_spec,
--- tests/test_validator.py
+++ tests/test_validator.py
@@ -5,5 +5,5 @@ from openapi21 import OPENAPI21_PATH, validate_spec_url
 
 def test_validate_spec_url():
     validate_spec_url(
-        'file:{}/example/swagger.json'.format(OPENAPI21_PATH)
+        'file:{}/spec/example/swagger.json'.format(OPENAPI21_PATH)
     )
